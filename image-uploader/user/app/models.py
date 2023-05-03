813f5f5ffdb7add905bd1393a637380fd55861ab
somesh-py <someshsharma615@gmail.com>
2023-05-03 20:51:05 +0530

    add images and create relationship between tow models using foreignkeyrelation

--- image-uploader/user/app/models.py
+++ image-uploader/user/app/models.py
@@ -1,15 +1,26 @@
 from tortoise.models import Model
-from tortoise import Tortoise,fields
-
+from tortoise import Tortoise, fields
 
 
 class Category(Model):
-    name=fields.CharField(200,unique=True)
-    slug=fields.CharField(200)
-    category_image=fields.TextField()
-    description=fields.TextField()
-    is_active=fields.BooleanField(default=True)
-    updated_at=fields.DatetimeField(auto_now=True)
-    created_at=fields.DatetimeField(auto_now_add=True)
+    name = fields.CharField(200, unique=True)
+    slug = fields.CharField(200)
+    category_image = fields.TextField()
+    description = fields.TextField()
+    is_active = fields.BooleanField(default=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
-Tortoise.init_models(["app.models"],"models")
+
+class Subcategory(Model):
+    Category=fields.ForeignKeyRelation(Category)
+    name = fields.CharField(200, unique=True)
+    slug = fields.CharField(200)
+    category_image = fields.TextField()
+    description = fields.TextField()
+    is_active = fields.BooleanField(default=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+
+Tortoise.init_models(["app.models"], "models")
--- image-uploader/user/app/routers.py
+++ image-uploader/user/app/routers.py
@@ -1,48 +1,50 @@
-from fastapi import APIRouter,Depends,UploadFile,File,status,HTTPException
+from fastapi import APIRouter, Depends, UploadFile, File, status, HTTPException
 from slugify import slugify
 from .models import Category
 from .pydentic_models import categoryitem
 import os
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
-router=APIRouter()
+router = APIRouter()
+
 
 @router.post("/category/")
-async def create_category(data:categoryitem=Depends(),
-                          category_image:UploadFile=File(...)):
+async def create_category(data: categoryitem = Depends(),
+                          category_image: UploadFile = File(...)):
     if await Category.exists(name=data.name):
-        return {"status":False,"message":"cartegory already exists"}
+        return {"status": False, "message": "cartegory already exists"}
     else:
-        slug=slugify(data.name)
-        FILEPATH="static/images/category"
+        slug = slugify(data.name)
+        FILEPATH = "static/images"
 
         if not os.path.isdir(FILEPATH):
             os.mkdir(FILEPATH)
 
-        filename=category_image.filename
-        extension=filename.split(".")[1]
-        imagename=filename.split(".")[0]
+        filename = category_image.filename
+        extension = filename.split(".")[1]
+        imagename = filename.split(".")[0]
 
-        if extension not in ["png","jpg","jpeg"]:
-            return {"status":"error","detail":"file extension not allowed"}
-        
-        dt=datetime.now()
-        dt_timestamp=round(datetime.timestamp(dt))
+        if extension not in ["png", "jpg", "jpeg"]:
+            return {"status": "error", "detail": "file extension not allowed"}
 
-        modefied_image_name=imagename+"_"+str(dt_timestamp)+"."+extension
-        generated_name=FILEPATH+modefied_image_name
-        # file_content=await category_image.read()
+        dt = datetime.now()
+        dt_timestamp = round(datetime.timestamp(dt))
 
-        # with open(generated_name,"wb") as file:
-        #     file.write(file_content)
+        modefied_image_name = imagename+"_"+str(dt_timestamp)+"."+extension
+        generated_name = FILEPATH+modefied_image_name
+        file_content = await category_image.read()
 
-        #     file.close()
-        
+        with open(generated_name, "wb") as file:
+            file.write(file_content)
+
+            file.close()
+
         # image_url=app_url+generated_name
 
-        category_obj= await Category.create(
+        category_obj = await Category.create(
             category_image=generated_name,
             description=data.description,
             name=data.name,
             slug=slug
         )
+        return category_obj
